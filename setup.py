7c3f98953c3807c37b7597f38fdba7c06abd1dc0
ale-rt <alessandro.pisa@gmail.com>
2025-04-01 07:44:02 +0200

    Back to development: 1.0.0a6

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.pdbpp",
-    version="1.0.0a5",
+    version="1.0.0a6.dev0",
     description="An addon for Plone that allow you debug your code using the pdbpp package",
     long_description=long_description,
     long_description_content_type="text/markdown",
