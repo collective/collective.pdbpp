1baf3586ba388501a13053091a5d52eea389a981
ale-rt <alessandro.pisa@gmail.com>
2026-02-06 09:48:03 +0100

    Update with plone.meta

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 """Installer for the collective.pdbpp package."""
 
-from setuptools import find_packages
 from setuptools import setup
-
 
 long_description = "\n\n".join(
     [
@@ -25,15 +23,17 @@ setup(
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: Addon",
-        "Framework :: Plone :: 5.2",
         "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: 6.1",
+        "Framework :: Plone :: 6.2",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Operating System :: OS Independent",
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
     keywords="Python Plone CMS",
     author="ale-rt",
@@ -45,13 +45,15 @@ setup(
         "Tracker": "https://github.com/collective/collective.pdbpp/issues",
     },
     license="GPL version 2",
-    packages=find_packages("src", exclude=["ez_setup"]),
-    namespace_packages=["collective"],
-    package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.8",
-    install_requires=["setuptools", "plone.api", "pdbpp", "rich"],
+    python_requires=">=3.9",
+    install_requires=[
+        "Products.CMFPlone",
+        "plone.api",
+        "pdbpp",
+        "rich",
+    ],
     extras_require={
         "test": [
             "plone.app.testing",
